0eab69ecf4e724d5175e7fe17aca88565458c325
robbiebit <robertbidinger@gmail.com>
2024-11-26 17:45:31 -0700

    Add Global Catalog disable option

--- ADToolkit.ps1
+++ ADToolkit.ps1
@@ -25,10 +25,11 @@
     Write-Host "1: Check FSMO role holders"
     Write-Host "2: Transfer FSMO roles"
     Write-Host "3: Check for Global Catalog servers"
-    Write-Host "4: Check AD functional level"
-    Write-Host "5: Raise AD functional level to Server 2016"
-    Write-Host "6: DC Diagnostics Report"
-    Write-Host "7: Check Domain Controller replication"
+    Write-Host "4: Disable Global Catalog function from Domain Controller"
+    Write-Host "5: Check AD functional level"
+    Write-Host "6: Raise AD functional level to Server 2016"
+    Write-Host "7: DC Diagnostics Report"
+    Write-Host "8: Check Domain Controller replication"
     Write-Host "Q: Press 'Q' to quit."
 }
 do
@@ -43,23 +44,27 @@ do
         "`nDomain Controller: "    + $_.Name
         "FSMO Roles Owned: "   + $_.OperationMasterRoles}
     } '2' {
-        Write-Host "To transfer FSMO roles you need to supply the destination DC hostname (i.e. gpsgpccdcs01)" -ForegroundColor Red
+        Write-Host "To transfer FSMO roles you need to supply the destination DC hostname (i.e. homelabdc, homelabdc2)" -ForegroundColor Red
         Move-ADDirectoryServerOperationMasterRole -OperationMasterRole DomainNamingMaster,PDCEmulator,RIDMaster,SchemaMaster,InfrastructureMaster
     } '3' {
         Write-Host  'Checking for Global Catalog Servers...' -ForegroundColor Green
       Get-ADForest  | FL GlobalCatalogs
     } '4' {
+        Write-Host 'Disable the Global Catalog function from Domain Controller' -ForegroundColor Green
+        $DomainController = Read-Host "Enter the name of the domain controller to disable Global Catalog"
+        repadmin.exe /options $DomainController –IS_GC
+    } '5' {
         Write-Host 'Checking for Active Directory Domain and Forest Functional Level...' -ForegroundColor Green
         Get-ADDomain | fl Name, DomainMode
         Get-ADForest | fl Name, ForestMode
-    } '5' {
+    } '6' {
         Write-Host 'Raising AD Domain and Forest to Server 2016 Functional Level...' -ForegroundColor Red
         Set-ADDomainMode -DomainMode 7
         Set-ADForestMode -ForestMode 7
-    } '6' {
+    } '7' {
         Write-Host 'Running DCDIAG...' -ForegroundColor Green
         dcdiag /v
-    } '7' {
+    } '8' {
         Write-Host 'Checking AD Replication...' -ForegroundColor Green
          repadmin /replsummary
     }
